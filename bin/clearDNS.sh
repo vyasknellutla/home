3281d66c581b7e919cad22b697f4e6bda02a079a
Vyas Nellutla <vyas@nifftytech.com>
2020-03-18 17:18:08 +0000

    format

--- bin/clearDNS.sh
+++ bin/clearDNS.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 ## TODO surround with "if macOS" conditional
-sudo dscacheutil -flushcache
-sudo killall -HUP mDNSResponder
-say cache flushed
+if [ "$(uname -s)" = "Darwin" ]; then # Check if using macOS
+    sudo dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+    say cache flushed
+elif [ "$(uname -s)" = "Linux" ]; then
+    sudo /etc/init.d/dns-clean restart
+    sudo /etc/init.d/networking force-reload
+fi
--- bin/uninstall-vsmac.sh
+++ bin/uninstall-vsmac.sh
@@ -4,54 +4,50 @@
 echo "Uninstalling Visual Studio for Mac..."
 
 sudo rm -rf "/Applications/Visual Studio.app"
-rm -rf ~/Library/Caches/VisualStudio
-rm -rf ~/Library/Preferences/VisualStudio
-rm -rf ~/Library/Preferences/Visual\ Studio
-rm -rf ~/Library/Logs/VisualStudio
-rm -rf ~/Library/VisualStudio
-rm -rf ~/Library/Preferences/Xamarin/
-rm -rf ~/Library/Application\ Support/VisualStudio
-rm -rf ~/Library/Application\ Support/VisualStudio/7.0/LocalInstall/Addins/
+rm -rf "${HOME}/Library/Caches/VisualStudio"
+rm -rf "${HOME}/Library/Preferences/VisualStudio"
+rm -rf "${HOME}/Library/Preferences/Visual Studio"
+rm -rf "${HOME}/Library/Logs/VisualStudio"
+rm -rf "${HOME}/Library/VisualStudio"
+rm -rf "${HOME}/Library/Preferences/Xamarin/"
+rm -rf "${HOME}/Library/Application Support/VisualStudio"
+rm -rf "${HOME}/Library/Application Support/VisualStudio/7.0/LocalInstall/Addins/"
 
 # Uninstall Xamarin.Android
 echo "Uninstalling Xamarin.Android..."
 
-sudo rm -rf /Developer/MonoDroid
-rm -rf ~/Library/MonoAndroid
+sudo rm -rf "/Developer/MonoDroid"
+rm -rf "${HOME}/Library/MonoAndroid"
 sudo pkgutil --forget com.xamarin.android.pkg
-sudo rm -rf /Library/Frameworks/Xamarin.Android.framework
-
+sudo rm -rf "/Library/Frameworks/Xamarin.Android.framework"
 
 # Uninstall Xamarin.iOS
 echo "Uninstalling Xamarin.iOS..."
 
-rm -rf ~/Library/MonoTouch
-sudo rm -rf /Library/Frameworks/Xamarin.iOS.framework
-sudo rm -rf /Developer/MonoTouch
+rm -rf "${HOME}/Library/MonoTouch"
+sudo rm -rf "/Library/Frameworks/Xamarin.iOS.framework"
+sudo rm -rf "/Developer/MonoTouch"
 sudo pkgutil --forget com.xamarin.monotouch.pkg
 sudo pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
-
 
 # Uninstall Xamarin.Mac
 echo "Uninstalling Xamarin.Mac..."
 
-sudo rm -rf /Library/Frameworks/Xamarin.Mac.framework
-rm -rf ~/Library/Xamarin.Mac
-
+sudo rm -rf "/Library/Frameworks/Xamarin.Mac.framework"
+rm -rf "${HOME}/Library/Xamarin.Mac"
 
 # Uninstall Workbooks and Inspector
 echo "Uninstalling Workbooks and Inspector..."
 
-sudo /Library/Frameworks/Xamarin.Interactive.framework/Versions/Current/uninstall
-
+sudo "/Library/Frameworks/Xamarin.Interactive.framework/Versions/Current/uninstall"
 
 # Uninstall the Visual Studio for Mac Installer
 echo "Uninstalling the Visual Studio for Mac Installer..."
 
-rm -rf ~/Library/Caches/XamarinInstaller/
-rm -rf ~/Library/Caches/VisualStudioInstaller/
-rm -rf ~/Library/Logs/XamarinInstaller/
-rm -rf ~/Library/Logs/VisualStudioInstaller/
+rm -rf "${HOME}/Library/Caches/XamarinInstaller/"
+rm -rf "${HOME}/Library/Caches/VisualStudioInstaller/"
+rm -rf "${HOME}/Library/Logs/XamarinInstaller/"
+rm -rf "${HOME}/Library/Logs/VisualStudioInstaller/"
 
 # Uninstall the Xamarin Profiler
 echo "Uninstalling the Xamarin Profiler..."
