70beed735fe8a09d9a7480c9c157105084ea61b5
Vyas Nellutla <vyasnellutla@gmail.com>
2021-01-12 00:13:05 -0600

    update

---
+++ bin/updateNix.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# MacOS
+sudo -i sh -c 'nix-channel --update && nix-env -iA nixpkgs.nix && launchctl remove org.nixos.nix-daemon && launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist'
